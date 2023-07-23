396673a649ff983d8c0c877e750776f13eef18e7
Ivan Polkanov <polkanovclassicpro@gmail.com>
2023-07-23 16:05:54 -0700

    Add base functional

--- TelegramFindLinkHelper/Program.cs
+++ TelegramFindLinkHelper/Program.cs
@@ -1,10 +1,35 @@
-﻿namespace TelegramFindLinkHelper
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using TelegramFindLinkHelper.Models;
+
+namespace TelegramFindLinkHelper
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                Console.WriteLine("Please enter filepath.");
+                var filePath = Console.ReadLine();
+
+                var exists = File.Exists(filePath);
+
+                if (!exists)
+                {
+                    Console.WriteLine("File has not been founded:");
+                    continue;
+                }
+
+                var jsonContent = File.ReadAllText(filePath);
+
+                var chatData = JsonSerializer.Deserialize<TelegramJSONChatExport>(jsonContent);
+                var messangesWithDate = chatData.messages.Select(x => (x.date, x.text_entities?.FirstOrDefault()?.text)).Where(x => x.text != null).ToArray();
+
+                Console.ReadLine();
+            }
         }
+
+
     }
 }
