f48329438526eece5b29361274c55623cc7cf89e
Ivan Polkanov <polkanovclassicpro@gmail.com>
2023-07-26 15:21:06 -0700

    Add basic logic for Parse + start CLI tooling

--- TelegramFindLinkHelper/Program.cs
+++ TelegramFindLinkHelper/Program.cs
@@ -1,4 +1,5 @@
-﻿using System.Text.Json;
+﻿using System.Text;
+using System.Text.Json;
 using System.Text.Json.Serialization;
 using TelegramFindLinkHelper.Models;
 
@@ -8,28 +9,33 @@ namespace TelegramFindLinkHelper
     {
         static void Main(string[] args)
         {
-            while (true)
+            var str = new StringBuilder();
+
+            foreach (var item in args)
             {
-                Console.WriteLine("Please enter filepath.");
-                var filePath = Console.ReadLine();
-
-                var exists = File.Exists(filePath);
-
-                if (!exists)
-                {
-                    Console.WriteLine("File has not been founded:");
-                    continue;
-                }
-
-                var jsonContent = File.ReadAllText(filePath);
-
-                var chatData = JsonSerializer.Deserialize<TelegramJSONChatExport>(jsonContent);
-                var messangesWithDate = chatData.messages.Select(x => (x.date, x.text_entities?.FirstOrDefault()?.text)).Where(x => x.text != null).ToArray();
-
-                Console.ReadLine();
+                str.Append(item);
             }
+
+            Console.WriteLine($"Your args are : \n\t\t\t {str.ToString()}");
+
+            //var exists = File.Exists(filePath);
+
+            //if (!exists)
+            //{
+            //    Console.WriteLine("File has not been founded:");
+            //    continue;
+            //}
+
+            //var jsonContent = File.ReadAllText(filePath);
+
+            //var chatData = JsonSerializer.Deserialize<TelegramJSONChatExport>(jsonContent);
+            //var messangesWithDate = chatData.messages.Select(x => (x.date, x.text_entities?.FirstOrDefault()?.text)).Where(x => x.text != null).ToArray();
+
+            //var messangesWithDateHttp = messangesWithDate.Where(x => x.text.StartsWith("http")).ToArray();
+
+            Console.ReadLine();
         }
-
-
     }
+
+
 }
